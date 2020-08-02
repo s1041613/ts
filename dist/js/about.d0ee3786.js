8c4120469b5f9f2d1a7dcbe25d06d48e0045468f
s1041613 <y10135124@gmail.com>
2020-08-02 19:40:15 +0800

    hw3:2020/07/31 題目 interface

--- dist/js/about.d0ee3786.js
+++
@@ -1,2 +0,0 @@
-(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["about"],{e70e:function(n,e,t){"use strict";t.r(e);var s=function(){var n=this,e=n.$createElement,t=n._self._c||e;return t("div",[n._v(n._s(n.msg))])},o=[],u=t("a6f4"),c=Object(u["b"])({name:"HelloTypescript",setup:function(){var n;(function(n){n[n["x"]=5]="x",n["y"]="6"})(n||(n={}));n.x;var e="hi my name is jenny";function t(n){console.log("msg",n)}return t(e),{msg:e}}}),a=c,i=t("2877"),l=Object(i["a"])(a,s,o,!1,null,null,null);e["default"]=l.exports}}]);
-//# sourceMappingURL=about.d0ee3786.js.map
--- src/views/HelloTypescript.vue
+++ src/views/HelloTypescript.vue
@@ -14,6 +14,66 @@ import {
 export default defineComponent({
   name: "HelloTypescript",
   setup() {
+    //hw3:2020/07/31 題目 interface
+    interface K {
+      x: number,
+      y: string
+    }
+    let k:K = { x: 5, y: '6'}
+
+    interface L {
+      x: number,
+      y: string,
+      z?:boolean
+    }
+    let l:L = { x: 5, y: '6'}
+    l.z = true
+
+    interface C {
+      [index: number]: number | string
+    } 
+    let cc:C = [1, '2'] // 此題類似I
+
+    interface D {
+      (arg0: number):number
+      //ans:(arg0: string):number
+    } 
+    let dd:D = function (arg0) {
+        return Number(arg0)
+    }
+
+    interface E {
+      (arg0: string, arg1: string):string,
+    } 
+    let ee:E = function (arg0, arg1):string {
+        return arg0 + arg1
+    }
+
+    interface F {
+      ():string
+    }    
+    let ff:F = () => 'Hello'
+   
+    interface G {
+      (x:number, y: number):number
+    } 
+    let gg:G = (x:number, y: number) => x + y
+
+    interface H {
+      // myAns:(cb:(x:number):void):void
+      (cb:(x:number)=>void):void
+    } 
+    let cb = function (x: number) {
+        cb(x + 1)
+    }
+    let hh:H = (cb) => {}
+
+    interface I {
+      [key: number]: string
+    } 
+    let ii:I = {} // 這個 object 的 key 可以是任意整數，value為字串
+
+
     //hw2:2020/07/24 題目 基礎型別
     let a:number = 10
     let b:string = '10'
@@ -25,7 +85,7 @@ export default defineComponent({
     //Tuple:用來表示一個已知元素數量和型別的陣列 
     //notes: 宣告成["1", 2]會錯
     let h:[number, string] = [1, '2']
-    //ans:let h:Array<number||string> = [1, '2']
+    //ans:let h:Array<number|string> = [1, '2']
     let i:{x:number, y:string} = {x : 5, y : "6"}
     //Enum:表示被限定在一定範圍集合的系列元素 ex:紅綠燈(限定顏色紅、黃、綠)
     enum twoD {x = 5, y = "6"}
